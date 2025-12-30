5e6e88a23a9bf9b46548dade04bee3e318db0775
bsttiv <bastian.valencia@ug.uchile.cl>
2025-12-29 23:05:00 -0300

    Feat: Modified Scanner struct and its constructor

--- src/scanner/mod.rs
+++ src/scanner/mod.rs
@@ -1,8 +1,7 @@
-use std::{net::Ipv4Addr, sync::mpsc::{Receiver, Sender}};
+use std::{net::{IpAddr, Ipv4Addr}, ops::Range, str::FromStr, sync::mpsc::{Receiver, Sender}};
 
-use pnet::datalink::NetworkInterface;
 
-use super::cli::ScannerArgs;
+use super::cli::{ScannerArgs, utils::IntOrRange};
 use self::{receiver::ScannerReceiver, sender::ScannerSender, utils::new_transport};
 
 mod packet;
@@ -16,7 +15,9 @@ pub(super) struct Scanner{
     receiver: ScannerReceiver,
     rx_results: Receiver<bool>,
     tx_target: Sender<(Ipv4Addr, u16)>,
-    interface: NetworkInterface
+    target: Ipv4Addr,
+    source_ips: Vec<Ipv4Addr>,
+    ports: Vec<u16>
 }
 
 #[allow(dead_code)]
@@ -28,12 +29,33 @@ impl Scanner{
         let (ts, tr) = new_transport();
         let s = ScannerSender::new(ts, rx_target);
         let r = ScannerReceiver::new(tr, tx_results);
+        let ports = match args.port{
+            IntOrRange::Int(p) => vec![p],
+            IntOrRange::Range(start, end) => Range{start, end:end+1}.collect()
+        };
+        let src = args.interface.ips.first().expect("Error: Selected interface has no IPs").ip();
+        let src = match src{
+            IpAddr::V4(ip) => ip,
+            IpAddr::V6(_) => panic!("Error: IPv6 is not supported")
+        };
+        let source_ips = match args.decoy{
+            Some(d) => {
+                let mut v:Vec<Ipv4Addr> = d.iter().map(|a|Ipv4Addr::from_str(a).unwrap()).collect();
+                if !v.contains(&src){
+                    v.push(src);
+                }
+                v
+            },
+            None => {vec![src]}
+        };
         Scanner { 
             sender: s,
             receiver: r,
             rx_results,
             tx_target,
-            interface: args.interface
+            target: args.target,
+            source_ips,
+            ports
         }
     }
 }

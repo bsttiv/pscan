3b41f1624c032e8b0410d7c86a869c785df41d7d
bsttiv <bastian.valencia@ug.uchile.cl>
2025-12-30 20:20:01 -0300

    Feat: Added #[allow(dead_code)] in ScannerSender::send

--- src/scanner/sender.rs
+++ src/scanner/sender.rs
@@ -14,6 +14,7 @@ impl ScannerSender{
     pub(super) fn new(ts: TransportSender, rx_target: Receiver<(Ipv4Addr, u16)>) -> Self{
         ScannerSender { transport_sender: ts, rx_target }
     }
+    #[allow(dead_code)]
     pub(super) fn send(&mut self, sources: Vec<Ipv4Addr>){
         for (ip, port) in self.rx_target.iter(){
             for source in sources.iter(){
